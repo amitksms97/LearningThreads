aba9bbffbdde57fe11adc817645f07376b07c0c1
amitksms97 <amitksms97@gmail.com>
2021-05-01 18:06:45 +0530

    Add project files.

---
+++ LearningThreads/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace LearningThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mythread = new Thread(new ThreadStart(Work));
+
+            mythread.Start();
+
+            Console.ReadLine();
+        }
+
+        static void Work()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Work Complete");
+        }
+    }
+}
